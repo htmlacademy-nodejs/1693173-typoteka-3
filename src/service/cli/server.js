d970ffda1c99dfea64194f3010b24965d64ff56c
Ruslan <ruslan.shkolnykh@protonmail.com>
2021-09-05 11:56:59 -0700

    Перенес app. в run

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -16,25 +16,26 @@ const HttpCode = {
   UNAUTHORIZED: 401,
 };
 
-app.use(express.json());
-
-app.get(`/posts`, async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(MOCK_FILE_PATH);
-    const mocks = JSON.parse(fileContent);
-    res.json(mocks);
-  } catch (_err) {
-    res.send([]);
-  }
-});
-
-app.use((req, res) => res
-  .status(HttpCode.NOT_FOUND)
-  .send(`Not found`));
 
 module.exports = {
   name: `--server`,
   run(args) {
+    app.use(express.json());
+
+    app.get(`/posts`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(MOCK_FILE_PATH);
+        const mocks = JSON.parse(fileContent);
+        res.json(mocks);
+      } catch (_err) {
+        res.send([]);
+      }
+    });
+
+    app.use((req, res) => res
+      .status(HttpCode.NOT_FOUND)
+      .send(`Not found`));
+
     const serverPort = Number.parseInt(args, 10) || DEFAULT_PORT;
     app.listen(serverPort);
   }
